eaeeb0bd63750fbf8fc41e84af9c48c7e1602406
DanielRagusa12 <daniel.ragusa129@gmail.com>
2021-09-27 22:12:53 -0400

    Final Done

--- src/main/java/ex27/Ex27_Main.java
+++ src/main/java/ex27/Ex27_Main.java
@@ -1,4 +1,8 @@
 package ex27;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Daniel Ragusa
+ */
 
 import java.util.Scanner;
 
--- src/main/java/ex36/Ex36_Main.java
+++ src/main/java/ex36/Ex36_Main.java
@@ -1,4 +1,8 @@
 package ex36;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Daniel Ragusa
+ */
 
 import java.util.ArrayList;
 import java.util.Collections;
